555d81cea8cc0b72cdbd27a19a969c385c7a2bdc
John Sherrell <dev@ohn.sh>
2026-08-26 20:54:57 -0500

    Switch in link to dash.ohn.sh, replacing scratch.ohn.sh
    Also bump dependencies and remove cssnano, which I don't still use.

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,10 +1,6 @@
 const postcssJitProps = require('postcss-jit-props')
 const OpenProps = require('open-props')
-const cssnano = require('cssnano')
 
 module.exports = {
-  plugins: [
-    postcssJitProps(OpenProps),
-    cssnano(),
-  ]
+  plugins: [postcssJitProps(OpenProps)],
 }
